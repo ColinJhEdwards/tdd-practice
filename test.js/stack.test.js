14b913f13813c6203233214b755a682ea35fe94f
Colin Edwards <ColinJhEdwards@gmail.com>
2021-09-13 20:22:35 -0700

    added comments to better understand first test

--- test.js/stack.test.js
+++ test.js/stack.test.js
@@ -8,9 +8,14 @@ class Stack {
 describe("My stack", () => {
   // it/test individual tests
   it("is created empty", () => {
+    //since we are creating a new Stack we must create a stack class as seen above on line 1
     const stack = new Stack();
 
+    //since we are expecting stack.top to equal -1 we must assign
+    // the value to the stack class as seen on line 3
     expect(stack.top).toBe(-1);
+    //
+    expect(stack.items).toEqual({});
   });
 
   it.todo("can push to the top");
